31ceb974f971fdf09773c48159305443ea3192cd
Finde Xumara <lukastout@gmail.com>
2015-03-01 14:01:35 +0100

    Moved experiments, no root access on server

---
+++ YandexExperiment.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2015 Luka Stout and Finde Xumara
+#
+# Full copyright notice can be found in LICENSE.
+#
+import sys
+import os
+
+from click_models.DBN import DBN
+from click_models.DCM import DCM
+from click_models.SimpleDBN import SimpleDBN
+from click_models.SimpleDCM import SimpleDCM
+from click_models.UBM import UBM
+from click_models.TCM import TCM
+from click_models.FCM import FCM
+from click_models.VCM import VCM
+from session.Session import *
+
+from evaluation.Evaluation import *
+
+import time
+from reader import parse_yandex_sessions, parse_yandex_relevances
+
+from sklearn.cross_validation import train_test_split
+from tabulate import tabulate
+
+__author__ = 'Luka Stout and Finde Xumara'
+
+
+def main(sessions_file, relevance_file, n_sessions):
+    this_directory = os.path.dirname(os.path.realpath(__file__))
+
+    sessions_dict = parse_yandex_sessions(os.path.join(this_directory, sessions_file), int(n_sessions))
+
+    train, test = train_test_split(sessions_dict)
+
+    classes = [
+        UBM,
+        TCM,
+        SimpleDCM,
+        SimpleDBN,
+        DCM,
+        DBN
+    ]
+
+    headers = ['Model', 'LL', 'Perp', 'Rel.Pred.AUC', 'Ranking.NDCG', 'CTR Pred.', 'Comp. Time.']
+    tableData = []
+    true_relevances = parse_yandex_relevances(os.path.join(this_directory, relevance_file))  
+    rel_pred = RelevancePrediction(true_relevances)
+    ll = Loglikelihood()
+    perp = Perplexity()
+    ranking = RankingPerformance(true_relevances)
+    ctr_pred = CTRPrediction()
+
+    for click_model_class in classes:
+
+        print "==== %s ====" % click_model_class.__name__
+        click_model = click_model_class(click_model_class.get_prior_values())
+
+        start_time = time.time()
+        click_model.train(train)
+        training_time = (time.time() - start_time)
+        print("--- %s seconds ---" % training_time)
+
+        
+        print "Log-likelihood"  
+        log_likelihood = ll.evaluate(click_model, test)
+        print log_likelihood
+        
+        print "Perplexity"
+        perplexity, perplexity_at_rank = perp.evaluate(click_model, test)
+        print 'Total',perplexity
+        print 'At rank', " ".join(["%.4f" % v for v in perplexity_at_rank])
+        
+        print "Relevance prediction AUC"
+        rel_score = rel_pred.evaluate(click_model, test)
+        print rel_score
+
+        print "Relevance Ranking"
+        rank_score = ranking.evaluate(click_model, test)
+        print rank_score
+        
+        print 'Click through rate prediction MSE'
+        ctr_score = ctr_pred.evaluate(click_model, train+test)
+        print ctr_score
+        print ""
+
+        tableData.append([click_model_class.__name__, log_likelihood, perplexity, rel_score, rank_score, ctr_score, training_time])
+
+    _format = 'grid'
+    print '\n\nSUMMARY\n=========='
+    print 'Number of queries: ', n_sessions 
+    print tabulate(tableData, headers, tablefmt=_format)
+
+
+if __name__ == '__main__':
+    session_data = sys.argv[1]
+    relevance_data = sys.argv[2]
+    if len(sys.argv) >= 4:
+        sessions = sys.argv[3]
+    else:
+        sessions = 1000
+    main(session_data, relevance_data, sessions)
---
+++ reader.py
@@ -0,0 +1,97 @@
+import sys
+from session.Session import Session, Result
+import numpy as np
+from collections import defaultdict
+import pprint
+
+
+def transform_to_tasks(sessions):
+    # time_interval = 430  NOTE(Luka): Taken from Piwowarski 2009, Mining User Web Search Activity... according to TCM Paper
+    # Should be in seconds however Yandex click log does not add this information..
+    # This means all queries with same session_id will be a task.
+    # Also have no way to measure similarity between queries.
+    # However we can assume that Yandex has made session_ids already into tasks according to website
+    return sessions.values()
+
+
+def parse_yandex_sessions(sessions_filename, max_sessions=None):
+    """
+        Parse Yandex-sessions formatted as found here: 'http://imat-relpred.yandex.ru/en/datasets'
+
+        Two possible formats:
+
+        Query action
+        SessionID TimePassed TypeOfAction QueryID RegionID ListOfURLs
+
+        Click action
+        SessionID TimePassed TypeOfAction URLID
+
+        Returns list of sessions
+
+    """
+    sessions_file = open(sessions_filename, "r")
+    session_dict = defaultdict(list)
+    count = 0
+    another_counter = 0
+    for line_n, line in enumerate(sessions_file):
+
+        # NOTE(Luka): Limit the amount of lines to ease the development work because the whole file has 14.5 Million Sessions-lines and 19.5 Million Clicks-lines.
+        session_str = line.strip().split('\t')
+        session_id = session_str[0]
+        time = session_str[1]
+        type_action = session_str[2]
+        if type_action is "Q":
+            query = session_str[3]
+            region = session_str[4]
+            docs = session_str[5:]
+
+            session = Session(session_id, query, region = region)
+            for doc in docs:
+                web_result = Result(doc, -1, 0)
+                session.add_web_result(web_result)
+
+            if count >= max_sessions:
+                break
+            
+            
+            if session_dict[session_id]:
+                last_session_same_id = session_dict[session_id][-1]
+                if last_session_same_id.query  == session.query:
+                    if [web.object for web in last_session_same_id.web_results] == [web.object for web in session.web_results]:
+                       continue
+
+            session_dict[session_id].append(session)
+            count += 1
+
+
+        if type_action is "C":
+            doc = session_str[3]
+            #NOTE(Luka): Only look at last session, don't look at session_id
+            for result in session_dict[session_id][-1].web_results:
+                if result.object == doc:
+                    result.click = 1
+
+                    #print "Click found!"
+                    break
+            else:
+                #NOTE(Luka): Click not found might be artifact or because clicked result is after clipped results.
+                #print "Could not find {} for session {}".format(doc,session_id)
+                pass
+    
+    return [s for t in session_dict.values() for s in t]
+
+def parse_yandex_relevances(relevances_filename):
+    relevances = dict()
+    with open(relevances_filename, "r") as f:
+        for row in f:
+            query_id, region_id, url, rel = row.split('\t')
+            rel = float(rel.strip())
+            if query_id not in relevances:
+                relevances[query_id] = dict()
+            query = relevances[query_id]             
+            
+            query[url] = max(rel, query.get(url,0))
+    
+    return relevances
+
+
